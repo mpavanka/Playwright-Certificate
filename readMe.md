e22ba95c1c270721baccf8c05d828f60bb5ee9d7
mpavanka <pavankalyan68335@gmail.com>
2024-11-19 11:47:41 +0530

    updated

--- readMe.md
+++ readMe.md
@@ -1,4 +1,5 @@
-<h1>Steps to execute Test cases in</h1>
-<p>Step1: export -DBrowserType=chrome or webkit</p>
-<p>Step2: export -DheadLess=true or false</p>
-<p>Step3: run command in task ./gradlew test</p>
+# Steps to execute Test cases in
+
+* open git bash and run below commands
+* Step1: export BrowserType=chrome or webkit
+* Step3: run command in task ./gradlew test
--- src/test/java/context/commonconfigs.java
+++ src/test/java/context/commonconfigs.java
@@ -2,6 +2,7 @@ package context;
 
 import com.google.gson.JsonObject;
 import com.microsoft.playwright.*;
+import io.cucumber.java.Before;
 
 import java.net.URLEncoder;
 
@@ -10,30 +11,18 @@ public class commonconfigs {
     private Page page;
     private String cdpUrl;
     private Browser browser;
-    private BrowserContext context;
     private Playwright playwright;
-    private BrowserType browserType;
     private final String username = "pavankalyan68335";
     private final String accessKey = "CtSOjmjSWEH8nb5P1izMThNjbHdGVC2fcoZ2Is3HtKDlxAieCK";
 
+    @Before
     public void openBrowser() {
         playwright = Playwright.create();
         lamdaSetup();
-
-        String browserTypeString = System.getProperty("BrowserType", "chrome"); // Default to chrome
+        String browserTypeString = System.getenv("BrowserType");
         System.out.println("BrowserType: " + browserTypeString);
-
-        boolean headless = Boolean.parseBoolean(System.getProperty("headLess", "true")); // Default to headless
-
-        browserType = getBrowserType(browserTypeString);
-        if (cdpUrl != null) {
-            browser = browserType.connect(cdpUrl);
-        } else {
-            browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(headless));
-        }
-
-        context = browser.newContext();
-        page = context.newPage();
+        browser = getBrowserType(browserTypeString).connect(cdpUrl);
+        page = browser.newPage();
     }
 
     private BrowserType getBrowserType(String browserTypeString) {
@@ -69,13 +58,11 @@ public class commonconfigs {
             cdpUrl = "wss://cdp.lambdatest.com/playwright?capabilities=" + URLEncoder.encode(capabilities.toString(), "utf-8");
         } catch (Exception e) {
             System.err.println("Failed to encode LambdaTest capabilities: " + e.getMessage());
-            cdpUrl = null; // Fallback to local execution
+            cdpUrl = null;
         }
     }
 
     public void teardown() {
-        if (page != null) page.close();
-        if (context != null) context.close();
         if (browser != null) browser.close();
         if (playwright != null) playwright.close();
     }
